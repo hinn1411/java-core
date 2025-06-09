64b4fe82cb416ec9778b1f7fccc8a86824e99f6a
Hien Giang <quachtrieuvu@gmail.com>
2025-06-09 23:00:02 +0700

    learn: final

---
+++ Final.java
@@ -0,0 +1,9 @@
+public class Final {
+    public static int functionWithFinalParams(final int x, final int z) {
+        return x + z;
+    }
+    public static void main(String[] args) {
+        int res = functionWithFinalParams(1, 2);
+        System.out.println(res);
+    }
+}
