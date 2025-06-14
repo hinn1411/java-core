ffc0166d1120080c110fced42caf25b69ddc0ed7
Hien Giang <quachtrieuvu@gmail.com>
2025-06-14 23:27:32 +0700

    learn: annotation

---
+++ Annotation.java
@@ -0,0 +1,36 @@
+import java.lang.annotation.*;
+
+@Deprecated
+/*
+    @deprecated Use NewComponent instead
+ */
+class DeprecatedComponent {
+    // Compiler will show warning when using this class
+}
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface MyAnnotation {
+    String value() default "";
+    String name();
+    int age();
+    String[] newNames();
+}
+
+@MyAnnotation(name = "Hien Giang", age = 23, newNames = {"James", "Taki"})
+class MyClass {
+
+}
+
+
+class MyChildClass extends MyClass {
+// Child class inherits @MyAnnotation because of @Inherited is declared
+}
+
+public class Annotation {
+    public static void main(String[] args) {
+        DeprecatedComponent d = new DeprecatedComponent();
+    }
+}
