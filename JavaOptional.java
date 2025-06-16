b4d17252984b6b748887afb7b4bc80bb582cfda1
Hien Giang <quachtrieuvu@gmail.com>
2025-06-16 23:45:02 +0700

    learn: optional

--- JavaOptional.java
+++ JavaOptional.java
@@ -1,3 +1,7 @@
+import javax.swing.text.html.Option;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class JavaOptional {
@@ -23,8 +27,39 @@ public class JavaOptional {
         System.out.println(optFullName.orElse("Dummy full name"));
         System.out.println(Optional.ofNullable(null).orElse("Dummny full name"));
 
-//        System.out.println(Optional.ofNullable(null).orElseThrow(IllegalArgumentException::new));
+        try {
+            Optional.ofNullable(null).orElseThrow(IllegalArgumentException::new);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.toString());
+        }
 
-        System.out.println(Optional.ofNullable(null).get());
+        try {
+            Optional.ofNullable(null).get();
+        } catch (NoSuchElementException e) {
+            System.out.println(e.toString());
+        }
+
+        Integer year = 2025;
+        System.out.println(Optional.of(year).filter(y -> y == 2025).isPresent());
+        System.out.println(Optional.of(year).filter(y -> y == 2026).isPresent());
+
+        Integer computerPrice = 20;
+        boolean isInBudgetRange = Optional.of(computerPrice).filter(p -> 15 <= p && p <= 25).isPresent();
+        System.out.println("Is in budget range: " + isInBudgetRange);
+
+        List<String> fruits = Arrays.asList("Banana", "Apple", "Mango");
+        int totalOfFruits = Optional.of(fruits).map(List::size).orElseGet(() -> 0);
+        System.out.println("total fruits = " + totalOfFruits);
+
+        boolean isPasswordValid = Optional.of(" Hien Gianng ")
+                .map(String::trim)
+                .filter(p -> p.equals("Giang Tuan Hien"))
+                .isPresent();
+        System.out.println("Is password valid: " + isPasswordValid);
+
+        String defaultStr = "default str";
+        Optional<String> nullStr = Optional.empty();
+        Optional<String> demoStr = Optional.of(defaultStr);
+        Optional<String> resStr = nullStr.or(() -> demoStr);
     }
 }
