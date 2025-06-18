18d4b08094e9b7e3f98a090ea33df6115aaacf55
Hien Giang <quachtrieuvu@gmail.com>
2025-06-18 23:22:42 +0700

    learn: dependency injection

---
+++ DependencyInjection.java
@@ -0,0 +1,25 @@
+class ClientComponent {
+    private MyDao myDao;
+
+    public ClientComponent(MyDao myDao) {
+        this.myDao = myDao;
+    }
+}
+
+class MyDao {
+    private DataSource dataSource;
+    public MyDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+}
+
+class DataSourceImpl implements DataSource {
+
+}
+
+interface DataSource {
+
+}
+
+public class DependencyInjection {
+}
